2ef774c719bd9b0426d9b5a40644cacb864a80fc
Bijena Dhewaju <bijenadhewaju181@gmail.com>
2026-07-30 03:13:16 +0545

    fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,9 +33,8 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
+    "127.0.0.1,localhost,skillmap-backend-eze6.onrender.com"
 ).split(",")
-
 
 # Application definition
 
